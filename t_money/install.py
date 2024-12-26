52dd60038f013edb32d79b81b2e3c0b493577424
Alon Ben Refael <alonbr@proton.me>
2024-12-26 12:40:54 +0200

    fixes

--- t_money/install.py
+++ t_money/install.py
@@ -19,15 +19,15 @@ def after_install():
 		script='',
 		style='';
 	""")
-	file = open(csv_file, 'r')
-	reader = csv.reader(file, delimiter=',')
-	for row in reader:
-		doc = frappe.new_doc('Asset Type List')
-		type = row[0]
-		doc.name = type
-		doc.asset_type = type
-		doc.percent = float(row[1])
-		doc.insert()
+	# file = open(csv_file, 'r')
+	# reader = csv.reader(file, delimiter=',')
+	# for row in reader:
+		# doc = frappe.new_doc('Asset Type List')
+		# type = row[0]
+		# doc.name = type
+		# doc.asset_type = type
+		# doc.percent = float(row[1])
+		# doc.insert()
 
 
 
