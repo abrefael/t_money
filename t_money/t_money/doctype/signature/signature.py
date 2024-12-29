3fc1bf97d3f8897a2bcac62def277b8ee10c0330
Alon Ben Refael <alonbr@proton.me>
2024-12-29 17:34:43 +0200

    Update signature.py

--- t_money/t_money/doctype/signature/signature.py
+++ t_money/t_money/doctype/signature/signature.py
@@ -1,9 +1,21 @@
 # Copyright (c) 2024, Alon Ben Refael and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class Signature(Document):
 	pass
+
+
+@frappe.whitelist()
+def update_template(f_uri):
+	import odfdo
+	from frappe import cstr
+	if f_uri.split('/')[1] == 'files':
+		f_uri = '/public/' + f_uri
+	try:
+		odfdo.Document(cstr(frappe.local.site) + f_uri)
+	except:
+		return 1
