855d76651747ce2a1e1d37aa1ddc4930832a56ff
Alon Ben Refael <alonbr@proton.me>
2025-01-06 16:04:53 +0200

    Update sales.py

--- t_money/t_money/doctype/sales/sales.py
+++ t_money/t_money/doctype/sales/sales.py
@@ -57,7 +57,7 @@ def Create_Quotation(q_num, objective, notes):
 	TARGET = q_num + ".odt"
 	f_uri = frappe.db.get_single_value("Signature", "reupload")
 	if f_uri == '' or f_uri is None:
-		document = Document("assets/t_money/template.odt")
+		document = Document("frontend/public/templates/template.odt")
 	else:
 		if f_uri.split('/')[1] == 'files':
 			document = cstr(frappe.local.site) + '/public/' + f_uri
