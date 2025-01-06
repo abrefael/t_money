994530e16aedf9bef51e1309274b2db6e947c6da
Alon Ben Refael <alonbr@proton.me>
2025-01-06 15:31:14 +0200

    Update signature.js

--- t_money/t_money/doctype/signature/signature.js
+++ t_money/t_money/doctype/signature/signature.js
@@ -5,7 +5,7 @@ frappe.ui.form.on("Signature", {
 	build(frm) {
 		frappe.call({method:'t_money.t_money.doctype.signature.signature.build_template'
         }).then(r => {
-            window.open('/assets/t_money/templates/template.pdf', '_blank').focus();
+            window.open('/public/templates/template.pdf', '_blank').focus();
 		});
 	}
 });
