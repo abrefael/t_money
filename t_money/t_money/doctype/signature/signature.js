655a13baae80a973ffafb4a3ffd919780c189f66
Alon Ben Refael <alonbr@proton.me>
2025-01-06 12:12:08 +0200

    Update signature.js

--- t_money/t_money/doctype/signature/signature.js
+++ t_money/t_money/doctype/signature/signature.js
@@ -1,8 +1,23 @@
 // Copyright (c) 2024, Alon Ben Refael and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Signature", {
-// 	refresh(frm) {
+frappe.ui.form.on("Signature", {
+	build(frm) {
+		frappe.call({method:'t_money.t_money.doctype.signature.signature.build_template'
+        }).then(r => {
+            window.open(`/assets/t_money/template.pdf`, '_blank').focus();
+		});
+	}
+});
 
-// 	},
-// });
+
+frappe.ui.form.on("Signature", {
+	reupload(frm) {
+		frappe.call({method:'t_money.t_money.doctype.signature.signature.update_template'
+		}).then(r => {
+            if (r.massage == 1){
+				frappe.throw(__('You need to use LibreOffice writer (.odt) or template (.ott) file'))
+			}
+        });
+	}
+});
