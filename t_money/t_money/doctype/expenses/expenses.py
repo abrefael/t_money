461ee810cc5c66387ed0e58e5bde538fb686b824
Alon Ben Refael <alonbr@proton.me>
2025-05-27 16:04:40 +0300

    Update expenses.py

--- t_money/t_money/doctype/expenses/expenses.py
+++ t_money/t_money/doctype/expenses/expenses.py
@@ -15,7 +15,7 @@ def add_expenss(fisc_year, actual_sum, sum_var, ex_type):
 	type_dic = {'משרדיות ואחזקה':'office','הוצאות רכב':'car','ביטוח מקצועי והשתלמויות':'insurance','קבלני משנה':'subconturctors','נסיעות (תחב"ץ)':'transport'}
 #	If there is no Income loss report already for the specific year, create it.
 	if not frappe.db.exists("Income Loss Report", fisc_year):
-		doc = frappe.new_doc({"doctype":"Income Loss Report"})
+		doc = frappe.new_doc("Income Loss Report")
 		doc.title = fisc_year
 		doc.insert(
 			ignore_permissions=True,
