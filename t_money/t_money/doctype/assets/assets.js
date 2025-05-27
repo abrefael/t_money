12d428afa5852ceeb84c8763cd5f133101efc101
Alon Ben Refael <alonbr@proton.me>
2025-05-27 09:46:54 +0300

    Update assets.js

--- t_money/t_money/doctype/assets/assets.js
+++ t_money/t_money/doctype/assets/assets.js
@@ -10,11 +10,11 @@
 frappe.ui.form.on('Assets', {
 	calculate(frm) {
 		if ((!frm.doc.fiscal_year) || ((frm.doc.original_price == 0) && (frm.doc.canges_price == 0))){
-            		frappe.throw(__('יש לציין שנת כספים לחישוב וגם מחיר מקורי ו/או מחיר שינויים'));
-            		return;
+			frappe.throw(__('יש לציין שנת כספים לחישוב וגם מחיר מקורי ו/או מחיר שינויים'));
+			return;
 		}
 		if (frm.doc.total == 0){
-		    frm.set_value ('total', frm.doc.original_price + frm.doc.canges_price);
+			frm.set_value ('total', frm.doc.original_price + frm.doc.canges_price);
 		}
 		var prev_total_loss = frm.doc.total_loss;
 		perform_calc(frm, prev_total_loss);
@@ -40,11 +40,20 @@ function perform_calc(frm, prev_total_loss){
 		var diff = purchase_date - start;
 		var oneDay = 1000 * 60 * 60 * 24;
 		var day = Math.floor(diff / oneDay);
-		frm.set_value ('loss_requested', frm.doc.total * (frm.doc.rate_loss / 100) * ((365-day) / 365));	
+		frm.set_value ('loss_requested', frm.doc.total * (frm.doc.rate_loss / 100) * ((365-day) / 365));
 	}
 	else{
 		frm.set_value ('loss_requested', frm.doc.total * frm.doc.rate_loss  / 100);
 	}
+	frappe.db.set_value(
+		'Income Loss Report',
+		String(frm.doc.fiscal_year),
+		'asset_loss',
+		frm.doc.loss_requested + frappe.db.get_value(
+			'Income Loss Report',
+			String(frm.doc.fiscal_year),
+			'asset_loss')
+		)
 	frm.set_value('total_loss', frm.doc.loss_requested + prev_total_loss);
 	frm.set_value('current_value', frm.doc.total - frm.doc.total_loss);
 }
