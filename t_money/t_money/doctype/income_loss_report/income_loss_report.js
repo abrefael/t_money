528d75dadd2baa2b2c3cd7dcf76e75f31dda3883
Alon Ben Refael <alonbr@proton.me>
2025-05-27 16:15:45 +0300

    Update income_loss_report.js

--- t_money/t_money/doctype/income_loss_report/income_loss_report.js
+++ t_money/t_money/doctype/income_loss_report/income_loss_report.js
@@ -4,25 +4,25 @@
 
 
 
-frappe.ui.form.on('Income Loss Report', {
-	calc(frm) {
-		var total_income = 0;
-		var incoms = frm.doc.items;
-//	Calculate the total incom from receipts.
-		for (let i = 0; i < incoms.length; i++) {
-			total_income += incoms[i];
-		}
-//	Calculate the total losses due to: expenses (office, car, subcontructors and insurenses)
-//	plus the travel expenses and the losses on assets.
-		var losses = frm.doc.office + frm.doc.insurance + frm.doc.subconturctors + frm.doc.travel + frm.doc.expense_head + frm.doc.transport + frm.doc.car + frm.doc.asset_loss;
-//	Calculate the "רווח נקי לא מתואם", whatever that means...
-		var net_non = total_income - losses;
-//	Calculate the "רווח נקי מתואם", whatever that means...
-		var net_adj = net_non + frm.doc.car_non;
-		frm.set_value('total_profit', total_income);
-		frm.set_value('profit_pre', net_non);
-		frm.set_value('profit_adj', net_adj);
-		frm.refresh();
-		frm.save();
-	}
-});
+//frappe.ui.form.on('Income Loss Report', {
+//	calc(frm) {
+//		var total_income = 0;
+//		var incoms = frm.doc.items;
+////	Calculate the total incom from receipts.
+//		for (let i = 0; i < incoms.length; i++) {
+//			total_income += incoms[i];
+//		}
+////	Calculate the total losses due to: expenses (office, car, subcontructors and insurenses)
+////	plus the travel expenses and the losses on assets.
+//		var losses = frm.doc.office + frm.doc.insurance + frm.doc.subconturctors + frm.doc.travel + frm.doc.expense_head + frm.doc.transport + frm.doc.car + frm.doc.asset_loss;
+////	Calculate the "רווח נקי לא מתואם", whatever that means...
+//		var net_non = total_income - losses;
+////	Calculate the "רווח נקי מתואם", whatever that means...
+//		var net_adj = net_non + frm.doc.car_non;
+//		frm.set_value('total_profit', total_income);
+//		frm.set_value('profit_pre', net_non);
+//		frm.set_value('profit_adj', net_adj);
+//		frm.refresh();
+//		frm.save();
+//	}
+//});
