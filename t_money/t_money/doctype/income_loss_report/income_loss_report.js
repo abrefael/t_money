6213e6381e1e5f346ae690d560138cef952948bb
Alon Ben Refael <alonbr@proton.me>
2025-05-27 16:31:53 +0300

    Update income_loss_report.js

--- t_money/t_money/doctype/income_loss_report/income_loss_report.js
+++ t_money/t_money/doctype/income_loss_report/income_loss_report.js
@@ -10,11 +10,11 @@ frappe.ui.form.on('Income Loss Report', {
 		var incoms = frm.doc.items;
 //	Calculate the total incom from receipts.
 		for (let i = 0; i < incoms.length; i++) {
-			total_income += incoms[i];
+			total_income += incoms[i].sum;
 		}
 //	Calculate the total losses due to: expenses (office, car, subcontructors and insurenses)
 //	plus the travel expenses and the losses on assets.
-		var losses = frm.doc.office + frm.doc.insurance + frm.doc.subconturctors + frm.doc.travel + frm.doc.expense_head + frm.doc.transport + frm.doc.car + frm.doc.asset_loss;
+		var losses = frm.doc.office + frm.doc.insurance + frm.doc.subconturctors + frm.doc.travel + frm.doc.transport + frm.doc.car + frm.doc.asset_loss;
 //	Calculate the "רווח נקי לא מתואם", whatever that means...
 		var net_non = total_income - losses;
 //	Calculate the "רווח נקי מתואם", whatever that means...
