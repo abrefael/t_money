e537d0ce37976b6973493f6889c9ce2c6afb4e81
Alon Ben Refael <alonbr@proton.me>
2025-05-28 11:35:55 +0300

    Update income_loss_report.js

--- t_money/t_money/doctype/income_loss_report/income_loss_report.js
+++ t_money/t_money/doctype/income_loss_report/income_loss_report.js
@@ -10,15 +10,15 @@ frappe.ui.form.on('Income Loss Report', {
 		var incoms = frm.doc.items;
 //	Calculate the total incom from receipts.
 		for (let i = 0; i < incoms.length; i++) {
-			total_income += incoms[i];
+			total_income += flt(incoms[i].sum);
 		}
 //	Calculate the total losses due to: expenses (office, car, subcontructors and insurenses)
 //	plus the travel expenses and the losses on assets.
-		var losses = frm.doc.office + frm.doc.insurance + frm.doc.subconturctors + frm.doc.travel + frm.doc.transport + frm.doc.car + frm.doc.asset_loss;
+		var losses = flt(frm.doc.office) + flt(frm.doc.insurance) + flt(frm.doc.subconturctors) + flt(frm.doc.travel) + flt(frm.doc.transport) + flt(frm.doc.car) + flt(frm.doc.asset_loss);
 //	Calculate the "רווח נקי לא מתואם", whatever that means...
 		var net_non = total_income - losses;
 //	Calculate the "רווח נקי מתואם", whatever that means...
-		var net_adj = net_non + frm.doc.car_non;
+		var net_adj = net_non + flt(frm.doc.car_non);
 		frm.set_value('total_profit', total_income);
 		frm.set_value('profit_pre', net_non);
 		frm.set_value('profit_adj', net_adj);
