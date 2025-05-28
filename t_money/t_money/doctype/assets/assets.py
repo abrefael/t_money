f32139d9a4837f2abaf5c819d9d9c71f8c26f7d9
Alon Ben Refael <alonbr@proton.me>
2025-05-28 16:38:47 +0300

    Update assets.py

--- t_money/t_money/doctype/assets/assets.py
+++ t_money/t_money/doctype/assets/assets.py
@@ -7,3 +7,12 @@ from frappe.model.document import Document
 
 class Assets(Document):
 	pass
+	
+	
+@frappe.whitelist()
+def del_frm(frm_name):
+	fisc_year, loss_requested = frappe.db.get_value('Assets', frm_name, ['fiscal_year','loss_requested'])
+	curr_val = frappe.db.get_value('Income Loss Report', fisc_year, 'asset_loss')
+	doc = frappe.get_doc('Income Loss Report', fisc_year)
+	doc.db_set('asset_loss', curr_val - loss_requested, commit=True)
+	frappe.delete_doc('Assets', frm_name)
