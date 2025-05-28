553b4c2e5111ddebf23ad6e4a3c963ba929d1bad
Alon Ben Refael <alonbr@proton.me>
2025-05-28 15:38:21 +0300

    Update expenses.py

--- t_money/t_money/doctype/expenses/expenses.py
+++ t_money/t_money/doctype/expenses/expenses.py
@@ -11,12 +11,14 @@ class Expenses(Document):
 
 
 @frappe.whitelist()
-def add_expenss(fisc_year, actual_sum, sum_var, ex_type):
+def add_expenss(fisc_year, actual_sum, sum_var, ex_type, old_sum, old_actual_sum, old_type, old_when):
 	type_dic = {'משרדיות ואחזקה':'office','הוצאות רכב':'car','ביטוח מקצועי והשתלמויות':'insurance','קבלני משנה':'subconturctors','נסיעות (תחב"ץ)':'transport'}
 	sum_var = float(sum_var)
 	actual_sum = float(actual_sum)
+	old_sum = float(old_sum)
+	ex_type = type_dic[ex_type]
 #	If there is no Income loss report already for the specific year, create it.
-	if not frappe.db.exists("Income Loss Report", fisc_year):
+	def create_doc(fisc_year):
 		doc = frappe.new_doc("Income Loss Report")
 		doc.insert(
 			ignore_permissions=True,
@@ -26,15 +28,74 @@ def add_expenss(fisc_year, actual_sum, sum_var, ex_type):
 		)
 		title = doc.get_title()
 		frappe.rename_doc('Income Loss Report', title, fisc_year)
-#		doc.db_set("name",fisc_year)
 		doc.db_set("year", int(fisc_year), commit=True)
-	doc = frappe.get_doc('Income Loss Report', fisc_year)
-	ex_type = type_dic[ex_type]
-	curr_val = frappe.db.get_value('Income Loss Report', fisc_year, ex_type)
+		return(doc)
+	
+	def add_it(fisc_year, ex_type,actual_sum,sum_var):
+		if not frappe.db.exists("Income Loss Report", fisc_year):
+			doc = create_doc(fisc_year)
+			curr_val = 0.0
+		else:
+			doc = frappe.get_doc('Income Loss Report', fisc_year)
+			curr_val = frappe.db.get_value('Income Loss Report', fisc_year, ex_type)
 #	Add the relative price payed to the relevant expense type
-	doc.db_set(ex_type, actual_sum + curr_val, commit=True)
-	if ex_type == 'car':
+		doc.db_set(ex_type, actual_sum + curr_val, commit=True)
+		if ex_type == 'car':
 #	adds the payments for car expenses to the "non-deductable car expenses"
-		curr_val = frappe.db.get_value('Income Loss Report', fisc_year, 'car_non')
-		doc.db_set('car_non', sum_var + curr_val - actual_sum, commit=True)
+			curr_val = frappe.db.get_value('Income Loss Report', fisc_year, 'car_non')
+			doc.db_set('car_non', sum_var + curr_val - actual_sum, commit=True)
+	if not old_type == '':
+		old_type = type_dic[old_type]
+		if not old_actual_sum == 0.0:
+			if not old_when == '': #User changed expense type & the price & the date of the invoice (different year)
+				add_it(old_when, old_type,(-1) * old_actual_sum,(-1) * old_sum)
+			else: #User changed expense type & the price on the invoice
+				add_it(fisc_year, old_type,(-1) * old_actual_sum,(-1) * old_sum)
+		else: #User changed expense type
+			add_it(fisc_year, old_type,(-1) * actual_sum,(-1) * sum_var)
+	elif not old_actual_sum == 0.0:
+		if not old_when == '': #User changed the price & the date of the invoice (different year)
+			add_it(old_when, ex_type,(-1) * old_actual_sum,(-1) * old_sum)
+		else: #User changed the price on the invoice
+			add_it(fisc_year, ex_type,(-1) * old_actual_sum,(-1) * old_sum)
+	elif not old_when == '': #User changed the date of the invoice (different year)
+		add_it(old_when, ex_type,(-1) * actual_sum,(-1) * old_sum)
+	add_it(fisc_year, ex_type,actual_sum,sum_var)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
