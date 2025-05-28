ef0fd53021268e3b380dbede879dfd8b9e843425
Alon Ben Refael <alonbr@proton.me>
2025-05-28 09:51:52 +0300

    Update expenses.py

--- t_money/t_money/doctype/expenses/expenses.py
+++ t_money/t_money/doctype/expenses/expenses.py
@@ -30,7 +30,7 @@ def add_expenss(fisc_year, actual_sum, sum_var, ex_type):
 	ex_type = type_dic[ex_type]
 	curr_val = frappe.db.get_value('Income Loss Report', fisc_year, ex_type)
 #	Add the relative price payed to the relevant expense type
-	doc.db_set(ex_type, curr_val + actual_sum, commit=True)
+	doc.db_set(ex_type, float(curr_val) + float(actual_sum), commit=True)
 	if ex_type == 'car':
 #	adds the payments for car expenses to the "non-deductable car expenses"
 		curr_val = frappe.db.get_value('Income Loss Report', fisc_year, 'car_non')
