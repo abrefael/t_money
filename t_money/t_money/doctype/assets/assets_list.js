2abbcb5a0d59c89c89afb04ab4c879777f33316a
Alon Ben Refael <alonbr@proton.me>
2025-05-29 11:58:00 +0300

    Update assets_list.js

--- t_money/t_money/doctype/assets/assets_list.js
+++ t_money/t_money/doctype/assets/assets_list.js
@@ -1,19 +1,25 @@
 frappe.listview_settings['Assets'].onload = function(listview) {
-    // add button to menu
-    listview.page.add_action_item(__("Test option"), function() {
-    	test( listview );
-});
-};
+    refresh: function(listview) {
+        listview.page.add_inner_button("Button Name", function() {
+            ButtonFunction(listview);
+        });
+    }
+ };
 
 function test( listview )
 {
-	let names=[];
-	$.each(listview.get_checked_items(), function(key, value) {
-		names.push(value.name);
-	});
-	if (names.length === 0) {
-		frappe.throw(__("No rows selected."));
-	}
-			
-	frappe.msgprint( names );
+    refresh: function(listview) {
+        listview.page.add_inner_button("Button Name", function() {
+            ButtonFunction(listview);
+        });;
+    },
+//	let names=[];
+//	$.each(listview.get_checked_items(), function(key, value) {
+//		names.push(value.name);
+//	});
+//	if (names.length === 0) {
+//		frappe.throw(__("No rows selected."));
+//	}
+//			
+//	frappe.msgprint( names );
 }
