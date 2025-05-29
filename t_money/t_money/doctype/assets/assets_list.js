a5d354eaaaa4c4505a6e700f86ebafd7551c1444
Alon Ben Refael <alonbr@proton.me>
2025-05-29 13:03:24 +0300

    Update assets_list.js

--- t_money/t_money/doctype/assets/assets_list.js
+++ t_money/t_money/doctype/assets/assets_list.js
@@ -1,10 +1,14 @@
 frappe.listview_settings['Assets'] = {
 
-    onload: function (listview) {
-
-        // Add a button for doing something useful.
-        listview.page.add_inner_button(__("Button"), function () {
-                        console.log("Yes");
+	onload: function (listview) {
+		listview.page.add_inner_button(__("Button"), function () {
+			console.log(listview.get_checked_items());
+			frappe.call({
+//				method:'t_money.t_money.doctype.assets.assets.del_frm',
+//				args: {
+//					frm_name: frm.doc.name
+//				}
+//			});
         })
 //        .addClass("btn-warning").css({'color':'darkred','font-weight': 'normal'});
         // The .addClass above is optional.  It just adds styles to the button.
