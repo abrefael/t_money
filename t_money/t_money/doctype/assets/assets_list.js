ff0c153199e1a4b103baa75980aa967642537a2d
Alon Ben Refael <alonbr@proton.me>
2025-05-29 13:08:50 +0300

    Update assets_list.js

--- t_money/t_money/doctype/assets/assets_list.js
+++ t_money/t_money/doctype/assets/assets_list.js
@@ -3,15 +3,14 @@ frappe.listview_settings['Assets'] = {
 	onload: function (listview) {
 		listview.page.add_inner_button(__("Button"), function () {
 			console.log(listview.get_checked_items());
-			frappe.call({
+//			frappe.call({
 //				method:'t_money.t_money.doctype.assets.assets.del_frm',
 //				args: {
 //					frm_name: frm.doc.name
 //				}
 //			});
-        });
 //        .addClass("btn-warning").css({'color':'darkred','font-weight': 'normal'});
         // The .addClass above is optional.  It just adds styles to the button.
     });
-};
+}
 }
