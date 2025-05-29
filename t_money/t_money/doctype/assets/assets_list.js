e7eff241da415ed6e7ac44e60ba191ad8a8025e5
Alon Ben Refael <alonbr@proton.me>
2025-05-29 12:59:33 +0300

    Update assets_list.js

--- t_money/t_money/doctype/assets/assets_list.js
+++ t_money/t_money/doctype/assets/assets_list.js
@@ -1,11 +1,12 @@
-function setupList({list}) {
-	return {
-		actions: [{
-			"label": "Delete Asset",
-			"onClick": () => {}
-		}],
-		bulk_actions: [
-			console.log('It worked!')
-		]
-	}
-}
+frappe.listview_settings['Assets'] = {
+
+    onload: function (listview) {
+
+        // Add a button for doing something useful.
+        listview.page.add_inner_button(__("Button"), function () {
+                        console.log("Yes");
+        })
+//        .addClass("btn-warning").css({'color':'darkred','font-weight': 'normal'});
+        // The .addClass above is optional.  It just adds styles to the button.
+    }
+};
