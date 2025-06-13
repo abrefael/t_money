ba58701422eaaa9a2c0bf882db48f1971045afd1
Alon Ben Refael <alonbr@proton.me>
2025-06-13 15:52:25 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -14,21 +14,24 @@ for entry in backups/*; do
    if [[ "${ans,,}" == "y" || -z "$ans" ]]; then
     get_backup_files+=" backups/$entry";
    else
-    :; # echo "I don't know yet";
+    echo 1; # echo "I don't know yet";
+    exit
    fi
   elif [[ "$entry" == *"private-files"* ]]; then
    read -p "$entry found. Is it a backup of private files? [Y/n] " ans;
    if [[ "${ans,,}" == "y" || -z "$ans" ]]; then
     get_backup_files=" --with-private-files backups/$entry$get_backup_files";
    else
-    :; # echo "I don't know yet";
+    echo 1; # echo "I don't know yet";
+    exit
    fi
   elif [[ "$entry" == *"files"* ]]; then
    read -p "$entry found. Is it a backup of public files? [Y/n] " ans;
    if [[ "${ans,,}" == "y" || -z "$ans" ]]; then
     get_backup_files=" --with-public-files backups/$entry $get_backup_files";
    else
-    :; # echo "I don't know yet";
+    echo 1; # echo "I don't know yet";
+    exit
    fi
   else
    if [ $i == 1 ]; then
@@ -58,12 +61,20 @@ for entry in backups/*; do
 if [ "$1" == "-h" ] || [ "$1" == "--help" ]; then
  cat << EOF
 
-usage: ./setup.su [-r][-h|--help]
+usage: ./setup.sh [-r][-h|--help]
+
 -r		activate site restore process, where you are prompted for
 		the files that contains the different backup files.
+		In order for you to perform a restore process, you will need:
+		1. A database backup file. It's name should be
+		{something}database{something else}.sql or .sql.gz.
+		2. Optional: A Public Files backup file. It's name should be
+		{something}files{something else}.tar or .tar.gz.";
+		It must *NOT* contain the string "private-files"!
+		3. Optional: A Private Files backup file. It's name should be
+		{something}private-files{something else}.tar or .tar.gz.
+		
 -h		Also --help, shows this prompt.
-
-
 
 EOF
  exit
@@ -85,7 +96,18 @@ if [ -z "$create_site" ] ; then
 fi
 
 if [ "$1" == "-r" ]; then
- echo "db_backup=\"$(get_backup_files)\"" >> .env;
+ get_backup_files=$(get_backup_files)
+ if [ $get_backup_files == 1]; then
+  echo "Please make sure you have up to three files for your backup:";
+  echo "1. A database backup file. It's name should be {something}database{something else}.sql or .sql.gz.";
+  echo "This is a mandatory file.";
+  echo "2. Optional: A Public Files backup file. It's name should be {something}files{something else}.tar or .tar.gz.";
+  echo "   It must *NOT* contain the string \"private-files\"!";
+  echo "3. Optional: A Private Files backup file. It's name should be {something}private-files{something else}.tar or .tar.gz.";
+  echo "Make sure you have the database file with a correct naming scheme at least, and restart the setup-restore process.";
+  exit
+ fi
+ echo "db_backup=\"$get_backup_files\"" >> .env;
 fi
 
 if [ -f Dockerfile ]; then 
