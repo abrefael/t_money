7c28bcb3cb25f3fc7404bb4a2371c6c0b3cebf79
Alon Ben Refael <alonbr@proton.me>
2026-02-05 15:44:52 +0200

    Update signature.py

--- t_money/t_money/doctype/signature/signature.py
+++ t_money/t_money/doctype/signature/signature.py
@@ -123,6 +123,7 @@ def build_template():
 	receipt_data = template.format(
 		date = '01/05/2028',
 		q_num = q_num,
+		op_num = op_num,
 		origin = origin,
 		client= "לקוח מספר 346",
 		h_p = h_p,
