e43a4a4aee6463f06562b869055f5d7d35798d60
Alon Ben Refael <alonbr@proton.me>
2026-02-06 07:30:08 +0200

    Update signature.py

--- t_money/t_money/doctype/signature/signature.py
+++ t_money/t_money/doctype/signature/signature.py
@@ -125,6 +125,7 @@ def build_template():
 		q_num = q_num,
 		email_add = email_add,
 		logo_img = logo_img,
+		signature = signature,
 		company_name = company_name,
 		phone_num = phone_num,
 		op_num = op_num,
