a2d8f28a17dc3283390404058884794a81797e93
Alon Ben Refael <alonbr@proton.me>
2026-02-06 16:52:52 +0200

    Update receipt.py

--- t_money/t_money/doctype/receipt/receipt.py
+++ t_money/t_money/doctype/receipt/receipt.py
@@ -147,7 +147,7 @@ def Create_Receipt(q_num, origin, fisc_year):
 		items_data = items_data,
 		company_name = company_name,
 		discount_segment = discount_segment,
-		total = f"{total:,.2f } ₪",
+		total = f"{total:,.2f }",
 		final = f"{final:,.0f}",
 		signature = signature,
 		notes = notes,
