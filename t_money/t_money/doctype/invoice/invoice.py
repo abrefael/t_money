ade1b1c9dcc3e50256f8510d65f1b8ade30a963d
Alon Ben Refael <alonbr@proton.me>
2026-02-06 17:00:03 +0200

    Update invoice.py

--- t_money/t_money/doctype/invoice/invoice.py
+++ t_money/t_money/doctype/invoice/invoice.py
@@ -37,9 +37,9 @@ def Create_Invoice(q_num):
 		item = f"""
 		<tr>
 			<td>{prod}</td>
-			<td>{description}</td>
+			<td>{desc}</td>
 			<td>{price:,.2f}</td>
-			<td>{quantity:,.1f}</td>
+			<td>{quant:,.1f}</td>
 			<td>{cost:,.2f} ₪</td>
 		</tr>
 		"""
@@ -61,7 +61,7 @@ def Create_Invoice(q_num):
 			ag_round = f"""
 	<tr>
 		<td class="one" >עיגול אגורות</td>
-		<td class="two" >{final:,.0f ₪}</td>
+		<td class="two" >{final:,.0f } ₪</td>
 	</tr>
 			"""
 			final = float(f"{final:,.0f}")
@@ -110,10 +110,10 @@ def Create_Invoice(q_num):
 	high_price = 0
 	most_impact = ''
 	for itm in itms:
-		prod = itm.item
-		desc = itm.desc
-		price = itm.price
-		quant = itm.quant
+		prod = itm["item"]
+		desc = itm["desc"]
+		price = itm["price"]
+		quant = itm["quant"]
 		cost = price * quant
 		if cost > high_price:
 			high_price = cost
@@ -131,7 +131,7 @@ def Create_Invoice(q_num):
 		logo_img = logo_img,
 		items_data = items_data,
 		discount_segment = discount_segment,
-		total = f"{total:,.2f ₪}",
+		total = f"{total:,.2f }",
 		final = f"{final:,.0f}",
 		op_num = op_num,
 		company_name = company_name,
