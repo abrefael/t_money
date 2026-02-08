2c3c90b570dbdbfcd31eff6b3e6f3128c7d12dd9
Alon Ben Refael <alonbr@proton.me>
2026-02-08 15:41:42 +0200

    Update receipt.py

--- t_money/t_money/doctype/receipt/receipt.py
+++ t_money/t_money/doctype/receipt/receipt.py
@@ -149,7 +149,7 @@ def Create_Receipt(q_num, origin, fisc_year):
 	)
 	TARGET = q_num + "(" + origin + ").pdf"
 	from weasyprint import HTML
-	pdf_bytes = HTML(string=receipt_data, base_url=".").write_pdf("sites/" + cstr(frappe.local.site) + "/public/" + TARGET)
+	pdf_bytes = HTML(string=receipt_data, base_url=".").write_pdf("sites/" + cstr(frappe.local.site) + "/public/files/" + TARGET)
 #	with open(cstr(frappe.local.site) + "/public/" + TARGET, "wb") as f:
 #		f.write(pdf_bytes)
 	file_doc = frappe.get_doc({
