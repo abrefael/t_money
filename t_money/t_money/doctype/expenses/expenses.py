20424c382831901d8d987af4d99cdb2fda7f14c9
Alon Ben Refael <alonbr@proton.me>
2026-02-09 11:25:09 +0200

    Update expenses.py

--- t_money/t_money/doctype/expenses/expenses.py
+++ t_money/t_money/doctype/expenses/expenses.py
@@ -66,7 +66,7 @@ def add_expenss(fisc_year, actual_sum, sum_var, ex_type, old_sum, old_actual_sum
 
 @frappe.whitelist()
 def get_doc_data(doc_name):
-	return frappe.get_doc("Expenses", doc_name)
+	return (frappe.get_doc("Expenses", doc_name))
 
 
 
