077a009c1ff40220f50ebaae10e8108e25deb9cf
Alon Ben Refael <alonbr@proton.me>
2026-02-09 09:53:43 +0200

    Create expenses_list.js

---
+++ t_money/t_money/doctype/expenses/expenses_list.js
@@ -0,0 +1,43 @@
+frappe.listview_settings.Expenses = {
+    onload: function (listview) {
+        listview.page.add_inner_button("Test", function () {
+            frappe.msgprint("Ping Pong");
+        });
+    }
+};
+
+//frappe.listview_settings['Expenses'] = {
+//	onload(listview) {
+//		if (!(window.location.href.includes('?'))) {
+//			window.location.assign(url);
+//		}
+//		listview.page.add_inner_button("הפעל פילטר שנתי", () => filter_year());
+//	}
+//};
+
+function filter_year()
+{
+const curr_year = new Date().getFullYear();
+const YEARS = String(curr_year+1) + '\n' + String(curr_year) + '\n' + String(curr_year-1) + '\n' + String(curr_year-2) + '\n' + String(curr_year-3) + '\n' + String(curr_year-4) + '\n' + String(curr_year-5) + '\n' + String(curr_year-6) + '\n' + String(curr_year-7);
+let d = new frappe.ui.Dialog({
+    title: 'בחר שנה',
+    fields: [
+        {
+            label: 'שנה לתצוגה',
+            fieldname: 'year',
+            default: curr_year,
+            fieldtype: 'select',
+            options: YEARS
+        }
+    ],
+    size: 'small', // small, large, extra-large 
+    primary_action_label: 'בחר',
+    primary_action(values) {
+        window.location.href = location.href.split("?")[0] + '?when=["Between"%2C["' + values.year + '-01-01"%2C"' + values.year + '-31-12"]]';
+        d.hide();
+    }
+});
+
+d.show();
+}
+
